996d5de759894c738483910d8eda662506f28700
EmrullahUcel <emrullahucel@gmail.com>
2023-03-15 19:52:57 +0300

    first

---
+++ sayi_tahmin.js
@@ -0,0 +1,23 @@
+var display = document.getElementById('display');
+var checkBox = document.getElementById('checkBox');
+var key = document.getElementsByClassName('key')
+var display2 = document.getElementById('display2')
+
+function onClickNumber(number){
+    display.value = display.value + number;
+};
+
+checkBox.onclick = function () {
+    var randomNumb = Math.floor(Math.random() * 11);
+    console.log(randomNumb);
+    console.log(parseInt(display.value))
+    if ((parseInt(display.value)) === randomNumb) {
+        display2.value = ("Tebrikler");
+        console.log("Tebrikler");
+        
+    } else
+        display2.value = ("Tekrar deneyin");
+        console.log("Tekrar deneyin");
+};
+
+
