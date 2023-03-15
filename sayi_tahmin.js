67fbc8b07c153f02c2058ad2a2b6f7038a3bbc19
EmrullahUcel <emrullahucel@gmail.com>
2023-03-15 20:29:03 +0300

    fixed bugs

--- sayi_tahmin.js
+++ sayi_tahmin.js
@@ -4,19 +4,20 @@ var key = document.getElementsByClassName('key')
 var display2 = document.getElementById('display2')
 
 function onClickNumber(number){
+    onClickNumber = number;
     display.value = display.value + number;
 };
 
 checkBox.onclick = function () {
-    var randomNumb = Math.floor(Math.random() * 11);
+    var randomNumb = Math.floor(Math.random() * 10);
     console.log(randomNumb);
     console.log(parseInt(display.value))
     if ((parseInt(display.value)) === randomNumb) {
-        display2.value = ("Tebrikler");
+        display2.value = ("Tebrikler Doğru sayı :" + randomNumb + " idi");
         console.log("Tebrikler");
         
     } else
-        display2.value = ("Tekrar deneyin");
+        display2.value = ("Tekrar deneyin Sayı : " + randomNumb + " idi");
         console.log("Tekrar deneyin");
 };
 
