ff4551dbf4b18d4aa60f3ef8409e1b8aed6029e1
EmrullahUcel <emrullahucel@gmail.com>
2023-03-16 17:08:49 +0300

    added scoreboard , changed colors

--- sayi_tahmin.js
+++ sayi_tahmin.js
@@ -2,7 +2,8 @@ var display = document.getElementById('display');
 var checkBox = document.getElementById('checkBox');
 var key = document.getElementsByClassName('key');
 var display2 = document.getElementById('display2');
-var score = [];
+var display3 = document.getElementById('display3');
+let score = 0;
 
 
 function onClickNumber(number) {
@@ -10,18 +11,24 @@ function onClickNumber(number) {
 };
 
 checkBox.onclick = function () {
-    var randomNumb = Math.floor(Math.random() * 10);
+    var randomNumb = Math.floor(Math.random() * 2);
     console.log(randomNumb);
 
     if ((parseInt(display.value)) === randomNumb) {
         display2.value = "Tebrikler Doğru sayı :" + randomNumb + " idi";
         console.log("Tebrikler");
         display.value = "";
-
-    } else
+        score = score + 10;
+        console.log(score);
+        display3.value = "Yeni skorunuz : " + score
+    } else {
         display2.value = "Tekrar deneyin Sayı : " + randomNumb + " idi";
         console.log("Tekrar deneyin");
         display.value = "";
+        score = score - 1;
+        console.log(score);
+        display3.value = "Yeni skorunuz : " + score
+    }
 };
 
 
